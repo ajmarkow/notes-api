5cad4239bc142ab6a183955c30d9eab3202624e8
AJ Markow <alexanderjmarkow@gmail.com>
2021-01-25 15:41:32 -0800

    Add helper methods for repository scores. Add imports for helper methods. Refactor object posted to dynamodb to include repository list and repository scores. Increased timeout to 2 minutes on AWS.

--- create.js
+++ create.js
@@ -1,23 +1,56 @@
+import fetch from "node-fetch";
 import * as uuid from "uuid";
-import handler from "./libs/handler-lib";
 import AWS from "aws-sdk";
+import repos from 'repos';
+
+const options = {
+  token: process.env.GIT_TOKEN,
+};
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+function addResponse(response, array) {
+  response.forEach((item) =>
+    array.push(item.full_name));
+    return array;
+};
 
-export async(event, context) {
+async function getGrades(inputArray) {
+	let Scores = [];
+	for (const item of inputArray) {
+	fetch(`http://readmescore.ajm.codes/score.json?url=${item}`)
+    .then((response) => Scores.push(response))
+    .catch((error) => {
+      let errorMessage = JSON.stringify(error);
+      console.log(errorMessage);
+    });  }
+	return Scores;
+};
+export async function main(event, context){
 //Request body is parsed as a json string
+	let listOfRepos = [];
+	let usernameParameter = event["queryStringParameters"]["username"];
 	const data = JSON.parse(event.body);
+	let returned_repos = await repos([`${usernameParameter}`], options)
+		.then((response) => addResponse(response, listOfRepos))
+		.catch((error) => {
+			let errorMessage = JSON.stringify(error);
+			console.log(errorMessage);
+		});
+	let repositoriesAsJSON = JSON.stringify(returned_repos);
+	let repositoriesScores = getGrades(repositoriesAsJSON);
 	const params = {
-		TableName:process.env.TableName,
-			Item: {
-			//Attributes of item being created by api
-			userId: "123", //author user id
-			noteId: uuid.v1(), //unique id for each
-			content: data.content,
-			attachment: data.attachment,
-			createdAt: Date.now(),
-			}
-		};
+    TableName: process.env.TableName,
+    Item: {
+      //Attributes of item being created by api
+      githubUsername: `${usernameParameter}`,
+      userId: "123", //author user id
+      repositories: `${repositoriesAsJSON}`,
+      repositories_scores: `${repositoriesScores}`,
+      gradeId: uuid.v1(), //unique id for each
+      attachment: data.attachment,
+      createdAt: Date.now(),
+    },
+  };
 
 	try {
 		await dynamoDb.put(params).promise();
@@ -30,3 +63,5 @@ export async(event, context) {
 			statusCode:500,
 			body:JSON.stringify({error: e.message})
 			};
+		}
+}
