44c59989e76f1f71837b6e1cffe88ab9dbedc8a2
AJ Markow <alexanderjmarkow@gmail.com>
2021-01-28 10:47:23 -0800

    Fix typos in delete.js

--- delete.js
+++ delete.js
@@ -3,7 +3,7 @@ import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const params = {
-    TableName: process.env.tableName,
+    TableName: process.env.TableName,
     Key: {
       userId: "123",
       gradeId: event.pathParameters.id,
