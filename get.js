ea663dc50061701f0326a32f90482dc0cdf50acd
AJ Markow <aj@ajm.codes>
2021-02-07 10:44:42 -0800

    Remove create and delete functions, update get endpoint to return correctly formatted JSON.

--- get.js
+++ get.js
@@ -1,20 +1,40 @@
+import fetch from "node-fetch";
 import handler from "./libs/handler-lib";
-import dynamoDb from "./libs/dynamodb-lib";
+import repos from 'repos';
+
+const options = {
+  token: process.env.GIT_TOKEN,
+};
+
+function addResponse(response, array) {
+  response.forEach((item) =>
+    array.push(item.full_name));
+    return array;
+};
+
+async function getGrades(repoSlug) {
+	//https://api.github.com/repos/ajmarkow/woodlandmist/community/profile
+		let repositoryScore = await fetch(`https://api.github.com/repos/${repoSlug}/community/profile`,{headers: {'Authorization':`token ${process.env.GIT_TOKEN}`}});
+		return repositoryScore.json();
+};
 
 export const main = handler(async (event, context) => {
-  const params = {
-    TableName: process.env.TableName,
-    // Key sets partition and sort key of item to get.
-    Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      gradeId: event.pathParameters.id,
-    },
-  };
-
-  const result = await dynamoDb.get(params);
-  if (!result.Item) {
-    throw new Error("Item not found!");
-  }
-
-  return result.Item;
+	//Request body is parsed as a json string
+	let listOfRepos = [];
+	let usernameParameter = event["queryStringParameters"]["username"];
+	let returned_repos = await repos([`${usernameParameter}`], options)
+		.then((response) => addResponse(response, listOfRepos))
+		.catch((error) => {
+			let errorMessage = JSON.stringify(error);
+			console.log(errorMessage);
+		});
+	let repositoriesScores = [];
+	returned_repos.forEach((item) => repositoriesScores.push(getGrades(item)));
+	let resolvedScores = await Promise.all(repositoriesScores);
+	let apiResponse = {
+		'githubUsername': `${usernameParameter}`,
+		'githubRepositories': returned_repos,
+		'readmeGrades': resolvedScores,
+	};
+	return apiResponse;
 });
